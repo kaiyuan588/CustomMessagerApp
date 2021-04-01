520d101f57261ae96886807f417095e5cd3d6595
kaiyuan zhao <kaiyuan941014@gmail.com>
2021-03-31 20:50:37 -0400

    creating login UI

---
+++ Messenger/Messenger/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by 低调 on 3/31/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    //MARK: -IBoutlets
+    @IBOutlet weak var emailLabelOutlet: UILabel!
+    @IBOutlet weak var passwordLabelOutlet: UILabel!
+    @IBOutlet weak var repeatPasswordLabelOutlet: UILabel!
+    
+    //MARK: -Labels
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
