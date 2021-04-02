02041b9945ace0eb92a4879e4ebb3b305680c794
kaiyuan zhao <kaiyuan941014@gmail.com>
2021-04-01 21:57:29 -0400

    save update profile to FB

--- Messenger/Messenger/EditProfileTableViewController.swift
+++ Messenger/Messenger/EditProfileTableViewController.swift
@@ -20,9 +20,17 @@ class EditProfileTableViewController: UITableViewController {
         super.viewDidLoad()
         
         tableView.tableFooterView = UIView()
-        showUserInfo()
+        configureTextField()
         
     }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showUserInfo()
+    }
+    
+    
     //MARK - TableView Delegate
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView();
@@ -31,9 +39,12 @@ class EditProfileTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return section == 0 ? 0.0 : 10.0
+        return section == 0 ? 0.0 : 30.0
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        //TODO: Show status view
         
     }
     
@@ -49,5 +60,30 @@ class EditProfileTableViewController: UITableViewController {
                 
             }
         }
+    }
+    
+    //MARK - Configure
+    private func configureTextField() {
+        usernameTextField.delegate = self
+        usernameTextField.clearButtonMode = .whileEditing
+    }
+}
+
+extension EditProfileTableViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == usernameTextField {
+            if textField.text != "" {
+                if var user = User.currentUser {
+                    user.userName = textField.text!
+                    saveUserLocally(user: user)
+                    FirebaseUserListenser.shared.saveUserToFirestore(user)
+                }
+            }
+            textField.resignFirstResponder()
+            return false
+        }
+        return true
     }
 }
